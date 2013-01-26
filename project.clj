2c31caaa970886c3f681d8ca8b3d6644b4db6b2a
Petter Eriksson <periksson@splunk.com>
2013-01-27 00:26:07 +0100

    Using petterik's clojure-mail 0.1.8

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [midje "1.5-alpha9"]
-                 [org.clojars.christianblunden/clojure-mail "0.1.8"]]
+                 [org.clojars.petterik/clojure-mail "0.1.8"]]
   :plugins [[lein-midje "3.0-alpha4"]])
